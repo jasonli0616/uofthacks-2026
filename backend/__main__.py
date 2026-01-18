c94e293778b482fa164a6acd89b999e73122b3a8
Jason Li <jasonli.0616@outlook.com>
2026-01-18 09:58:20 -0500

    Remove audio

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,15 +1,11 @@
 import time
 
 from .ml_chord_tracking import chord_tracking
-from ..pygame import audio
 
 def main(poll_interval=0.25):
 
     # Chord tracking
-    chord_tracking.start_in_background(show_windows=False)
-
-    # Start audio capture/processing
-    audio.start_audio_stream()
+    chord_tracking.start_in_background(show_windows=True)
 
     try:
         while True:
@@ -19,18 +15,12 @@ def main(poll_interval=0.25):
             if chord_prediction is not None:
                 print(chord_prediction)
 
-            # Pitch prediction
-            pitch_prediction = audio.get_pitch()  # returns note string or None
-            if pitch_prediction is not None:
-                print(pitch_prediction)
-
 
             time.sleep(poll_interval)
     except KeyboardInterrupt:
         pass
     finally:
         chord_tracking.stop_background()
-        audio.stop_audio_stream()
 
 if __name__ == "__main__":
     main()
